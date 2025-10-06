ddeadccc1a851f9c1df7a944880427545dd2bdd1
b00985701 <b00985701@M-MXB105-C21>
2025-10-06 15:01:13 +0100

    Added a constructor to book class

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    public Book(string bookTitle, string bookAuthor, string bookISBN)
+    {
+        Title = bookTitle;
+        Author = bookAuthor;
+        ISBN = bookISBN;
+    }
+
+    void DisplayInfo()
+    {
+        Console.WriteLine($"Book Title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+        book.DisplayInfo();
+    }
+}
