d50b0947c929e71c80aadaf9827ab972356b6d24
b00985701 <b00985701@M-MXB105-C21>
2025-10-06 15:11:14 +0100

    Updated codw with access modifiers.

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,24 +1,7 @@
-﻿class Book
+﻿using Library;
+
+class Program
 {
-    string Title;
-    string Author;
-    string ISBN;
-
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-    }
-
-    void DisplayInfo()
-    {
-        Console.WriteLine($"Book Title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
-
     static void Main(string[] args)
     {
         Book book = new Book("C# for beginners", "Bill Gates", "1234567");
