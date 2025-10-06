1ffd1cbdc97a575609dc3c0798305b17eb904faa
b00985701 <b00985701@M-MXB105-C21>
2025-10-06 15:15:02 +0100

    Updated code with variables and properties

--- Library/Book.cs
+++ Library/Book.cs
@@ -8,9 +8,25 @@ namespace Library
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title;
+        private string author;
+        private string isbn;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
 
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
